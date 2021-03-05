894af76acfb5867859522de3d923bb530ecfc771
Michael Hebesberger <michael.hebesberger@student.tugraz.at>
2021-03-05 19:12:55 +0100

    3rd level

---
+++ Assets/Scripts/Mechanics/Giftball.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Giftball : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("collide!");
+
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("hit it!");
+            GameObject.FindGameObjectWithTag("healthbar").GetComponent<Healthbar>().TakeDamage(30);
+            gameObject.SetActive(false);
+        }
+    }
+}
